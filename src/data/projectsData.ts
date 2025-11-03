1473e8eae3d8624231706b0b1cdf49d6a9b63a2f
Rahul Rajasekharan Menon <115494133+RahulRmCoder@users.noreply.github.com>
2025-11-04 00:30:20 +0530

    Update projectsData.ts

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -35,7 +35,7 @@ export const projectsData: Project[] = [
     codeLink: "https://github.com/RahulRmCoder/AI-File-Manager"
   },
   {
-    id: 3,
+    id: 4,
     title: "Word Weave: Blogging Website",
     description: "Word Weave – AI-powered modern blogging platform with a dark theme, intuitive writing, social engagement, and advanced analytics. ",
     image: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
@@ -43,7 +43,7 @@ export const projectsData: Project[] = [
     codeLink: "https://github.com/RahulRmCoder/Word-Weave-Blogging-Website"
   },
   {
-    id: 4,
+    id: 5,
     title: " CraveCrafters: Food Ordering Web Application",
     description: " CraveCrafters – AI-powered food ordering web app with a chatbot for menu browsing, orders & support. Built with Node.js, FastAPI, MongoDB & JS.",
     image: "https://media.istockphoto.com/id/2166408604/photo/group-adult-asia-male-female-freelance-typing-write-prompt-ai-bot-it-app-smart-program-nomad.webp?a=1&b=1&s=612x612&w=0&k=20&c=MVxFXRqN3d3UMU11rRitHUU6-KQ6lmIRf0GJAnVn-2k=",
@@ -51,7 +51,7 @@ export const projectsData: Project[] = [
     codeLink: "https://github.com/RahulRmCoder/CraveCrafters-Food-App"
   },
   {
-    id: 5,
+    id: 6,
     title: "GrammarGuru",
     description: "GrammarGuru is a web application that provides instant feedback on grammar, punctuation, and style to help users improve their writing. Whether you're drafting emails, writing content, or communicating in English as a second language, GrammarGuru helps you express yourself clearly and correctly.",
     image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29kaW5nfGVufDB8fDB8fHww",
